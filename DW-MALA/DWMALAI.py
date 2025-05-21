52ca09f7d091e472c9477ee0a3f9b8cb1a5a279f
AnonymousForConference <78520416+AnonymousForConference@users.noreply.github.com>
2025-05-22 00:32:12 +0900

    Delete DW-MALA directory

--- DW-MALA/DWMALAI.py
+++
@@ -1,9 +0,0 @@
-from VGG import build_vgg_model
-from dataloader.tiny import load_tiny
-from active_learning_loop import active_learning_loop
-
-print('imagenet start')
-
-(x_train, y_train), (x_test, y_test) = load_tiny()
-model = build_vgg_model()
-dwmala_train_acc, dwmala_test_acc = active_learning_loop(model, x_train, y_train, x_test, y_test)
